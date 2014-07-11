4625c2752e07c5952ad852817a2dea9eea6bed40
miyatad <miyatad@gmail.com>
2014-07-12 08:57:00 +0900

    first

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -1,6 +1,15 @@
 var app = {
     initialize: function() {
         // TODO: put PhoneGap initialization here
-        // alert("Hello World!");
+         alert("Hello World!");
+		 console.log("eeeee");
     },
 };
+
+function onBodyLoad(){
+	document.addEventListener("deviceready",onDeviceReady,false);
+}
+
+function onDeviceReady(){
+	console.log("rrrrr");
+}
